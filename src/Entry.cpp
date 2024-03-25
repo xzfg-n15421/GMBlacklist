3e7a3dab30518e21f34ea792262bc433400b3ca4
Tsubasa6848 <komatsu_moho@outlook.com>
2024-03-25 20:48:03 +0800

    adapt: adapt GMLIB 0.9.8

--- src/Entry.cpp
+++ src/Entry.cpp
@@ -1,25 +1,22 @@
 #include "Entry.h"
 #include "Global.h"
-#include <fmt/format.h>
-#include <functional>
-#include <ll/api/Config.h>
-#include <ll/api/io/FileUtils.h>
-#include <ll/api/plugin/NativePlugin.h>
-#include <ll/api/plugin/PluginManagerRegistry.h>
-#include <memory>
-#include <stdexcept>
+
 ll::Logger logger(PLUGIN_NAME);
 
 namespace GMBlacklist {
 
-namespace {
+std::unique_ptr<Entry>& Entry::getInstance() {
+    static std::unique_ptr<Entry> instance;
+    return instance;
+}
 
-std::unique_ptr<std::reference_wrapper<ll::plugin::NativePlugin>>
-    selfPluginInstance; // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
+bool Entry::load() {
+    initConfigFile();
+    checkBanTimeTask();
+    return true;
+}
 
-auto disable(ll::plugin::NativePlugin& /*self*/) -> bool { return true; }
-
-auto enable(ll::plugin::NativePlugin& /*self*/) -> bool {
+bool Entry::enable() {
     RegisterCommands();
     listenEvent();
     logger.info("GMBlacklist Loaded!");
@@ -28,34 +25,8 @@ auto enable(ll::plugin::NativePlugin& /*self*/) -> bool {
     return true;
 }
 
-auto load(ll::plugin::NativePlugin& self) -> bool {
-    selfPluginInstance = std::make_unique<std::reference_wrapper<ll::plugin::NativePlugin>>(self);
-    initConfigFile();
-    checkBanTimeTask();
-    return true;
-}
-
-auto unload(ll::plugin::NativePlugin& self) -> bool {
-    selfPluginInstance.reset();
-    return true;
-}
-
-} // namespace
-
-auto getSelfPluginInstance() -> ll::plugin::NativePlugin& {
-    if (!selfPluginInstance) {
-        throw std::runtime_error("selfPluginInstance is null");
-    }
-    return *selfPluginInstance;
-}
+bool Entry::disable() { return true; }
 
 } // namespace GMBlacklist
 
-extern "C" {
-_declspec(dllexport) auto ll_plugin_disable(ll::plugin::NativePlugin& self) -> bool {
-    return GMBlacklist::disable(self);
-}
-_declspec(dllexport) auto ll_plugin_enable(ll::plugin::NativePlugin& self) -> bool { return GMBlacklist::enable(self); }
-_declspec(dllexport) auto ll_plugin_load(ll::plugin::NativePlugin& self) -> bool { return GMBlacklist::load(self); }
-_declspec(dllexport) auto ll_plugin_unload(ll::plugin::NativePlugin& self) -> bool { return GMBlacklist::unload(self); }
-}
+LL_REGISTER_PLUGIN(GMBlacklist::Entry, GMBlacklist::Entry::getInstance());
--- src/Entry.h
+++ src/Entry.h
@@ -4,6 +4,30 @@
 
 namespace GMBlacklist {
 
-[[nodiscard]] auto getSelfPluginInstance() -> ll::plugin::NativePlugin&;
+class Entry {
+
+public:
+    static std::unique_ptr<Entry>& getInstance();
+
+    Entry(ll::plugin::NativePlugin& self) : mSelf(self) {}
+
+    [[nodiscard]] ll::plugin::NativePlugin& getSelf() const { return mSelf; }
+
+    /// @return True if the plugin is loaded successfully.
+    bool load();
+
+    /// @return True if the plugin is enabled successfully.
+    bool enable();
+
+    /// @return True if the plugin is disabled successfully.
+    bool disable();
+
+    // TODO: Implement this method if you need to unload the plugin.
+    // /// @return True if the plugin is unloaded successfully.
+    // bool unload();
+
+private:
+    ll::plugin::NativePlugin& mSelf;
+};
 
 } // namespace GMBlacklist
